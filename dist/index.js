f2eed27640e0862f9cd91d0d68326fd087b0a1f5
RishitMori <rishit1640@gmail.com>
2026-01-07 16:07:19 +0530

    [updated] idst file changes

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,11 @@
-const express = require("express");
-const app = express();
-app.use(express.json());
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
 // ✅ Express routes
 app.get("/health", (req, res) => {
     res.json({ status: "healthy" });
